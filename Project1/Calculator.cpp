f56b05cff07d76bf743a304b28256bf3a3b57341
GainAw <39569878+GainAw@users.noreply.github.com>
2019-02-11 21:36:44 -0800

    Update Calculator.cpp

--- Project1/Calculator.cpp
+++ Project1/Calculator.cpp
@@ -5,41 +5,37 @@ namespace Calculator
 	double current_value = 0.0;
 	double currentValue()
 	{
-		return current_value;
+		return (current_value);
 	}
 
 	double add(double left, double right)
 	{
 		current_value = left + right;
-		return (left + right);
+		return (current_value);
 	}
 	double sub(double left, double right)
 	{
 		current_value = left - right;
-		return (left - right);
+		return (current_value);
 	}
 	double mult(double left, double right)
 	{
 		current_value = left * right;
-		return (left * right);
+		return (current_value);
 	}
 	double div(double left, double right)
 	{
 		current_value = left / right;
-		return (left / right);
+		return (current_value);
 	}
 	double car(double left, double right)
 	{
 		current_value = pow(left, right);
-		return (pow(left, right));
+		return (current_value);
 	}
 	double invert()
 	{
 		current_value = current_value * -1.0;
-		return (current_value);
-	}
-	double memory()
-	{
 		return (current_value);
 	}
  }
